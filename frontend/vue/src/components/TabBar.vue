11865d25d77207af40fdc96db24354962e317a3a
MinjeMun <mmj010220@gmail.com>
2023-02-20 14:54:15 +0900

    임시 저장용

--- frontend/vue/src/components/TabBar.vue
+++ frontend/vue/src/components/TabBar.vue
@@ -1,20 +1,20 @@
 <template>
     <div id="menu-wrapper">
-            <table>
-                <tr>
-                    <th id="menu-tab"><a href=""><font-awesome-icon icon="fa-solid fa-bars" /></a></th>
-                    <th><a href="#">홈</a> </th>
-                    <th><a href="#">관심<br>종목</a></th>
-                    <th><a href="current.html">현재가</a></th>
-                    <th><a href="#">주식<br>주문</a></th>
-                    <th><a href="#">주식<br>잔고</a></th>
-                    <th><a href="#">종합<br>차트</a></th>
-                    <th><a href="#">지수환율<br>시장지표</a></th>
-                    <th><a href="#">매매<br>동향</a></th>
-                    <th><a href="#">즉시<br>이체</a></th>
-                    <th><a href="#">총자산<br>현황</a></th>
-                </tr>
-            </table>
+        <table>
+            <tr>
+                <th id="menu-tab"><a href=""><font-awesome-icon icon="fa-solid fa-bars" /></a></th>
+                <th><a href="#">홈</a> </th>
+                <th><a href="#">관심<br>종목</a></th>
+                <th><a href="current.html">현재가</a></th>
+                <th><a href="#">주식<br>주문</a></th>
+                <th><a href="#">주식<br>잔고</a></th>
+                <th><a href="#">종합<br>차트</a></th>
+                <th><a href="#">지수환율<br>시장지표</a></th>
+                <th><a href="#">매매<br>동향</a></th>
+                <th><a href="#">즉시<br>이체</a></th>
+                <th><a href="#">총자산<br>현황</a></th>
+            </tr>
+        </table>
     </div>
 </template>
 
