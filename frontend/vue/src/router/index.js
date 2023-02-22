f83c9ebc2a5b837bb0861b3d36ba3d3877c24f39
datadbs <dj9034kr@gmail.com>
2023-02-22 09:46:04 +0900

    feat: addd

--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -4,10 +4,12 @@ import MenuView from '../views/MenuView.vue'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
 import CurrentView from '../views/CurrentView.vue'
+import StockbuyView from '../views/StockbuyView.vue'
 import SearchView from '../views/SearchView.vue'
 import CartView from '../views/CartView.vue'
 import CreateView from '../views/CreateView.vue'
 import CartbuyView from '../views/CartbuyView.vue'
+import testView from '../views/testView.vue'
 
 const routes = [
   {
@@ -36,6 +38,11 @@ const routes = [
     component: CurrentView
   },
   {
+    path: '/stockbuy',
+    name: 'stockbuy',
+    component: StockbuyView
+  },
+  {
     path: '/search',
     name: 'search',
     component: SearchView
@@ -54,6 +61,11 @@ const routes = [
     path: '/cartbuy',
     name: 'cartbuy',
     component: CartbuyView
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: testView
   },
 ]
 
